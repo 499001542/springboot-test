f10cbb575485f58025dd42aaa17d6c40fb423c92
wujinming <499001542@qq.com>
2021-03-26 14:13:55 +0800

    打印 你好393939 信息

---
+++ src/main/java/com/softtek/domain/nihao.java
@@ -0,0 +1,12 @@
+package com.softtek.domain;
+
+/**
+ * @author : Bakey Wu
+ * @date : 2021-03-26-2:12 下午
+ */
+public class nihao {
+    public static void main(String[] args) {
+        System.out.println("你好393939");
+    }
+}
+
