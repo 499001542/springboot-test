dc5ac0175bad6e3b6255579e17e316f25b6b191a
wujinming <499001542@qq.com>
2021-03-26 14:24:37 +0800

    打印 tahao 信息

---
+++ src/main/java/com/softtek/domain/tahao.java
@@ -0,0 +1,11 @@
+package com.softtek.domain;
+
+/**
+ * @author : Bakey Wu
+ * @date : 2021-03-26-2:23 下午
+ */
+public class tahao {
+    public static void main(String[] args) {
+        System.out.println("tahao");
+    }
+}
