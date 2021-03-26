174aabada628f8421930d98ef86985bc098d2ed4
wujinming <499001542@qq.com>
2021-03-26 15:09:05 +0800

    打印 777 信息

---
+++ src/main/java/com/softtek/domain/tahaoter1.java
@@ -0,0 +1,11 @@
+package com.softtek.domain;
+
+/**
+ * @author : Bakey Wu
+ * @date : 2021-03-26-3:08 下午
+ */
+public class tahaoter1 {
+    public static void main(String[] args) {
+        System.out.println("777");
+    }
+}
